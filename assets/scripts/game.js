88c484bd9496eccf4fb014867f4d10f6dfaa25ec
[Github <[Github>
2023-01-08 22:43:07 +0900

    log출력 함수 for문으로 객체 키 다루기

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -186,7 +186,18 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  console.log(battleLog);
+  let i = 0;
+  for (const logEntry of battleLog) {
+    if ((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry === i) {
+      console.log(`#${i}`);
+      for (const key in logEntry) {
+        console.log(`${key} => ${logEntry[key]}`);
+      }
+      lastLoggedEntry = i;
+      break;
+    }
+    i++;
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
